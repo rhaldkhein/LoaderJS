10ce93d3bbb020c504d6b98b696be7a7db8ec53f
rhaldkhein <undyingmind@gmail.com>
2018-03-18 16:54:24 +0400

    Allow null items

--- loaderjs.js
+++ loaderjs.js
@@ -1,124 +1,127 @@
 // Imports
 var isWindow = typeof window !== 'undefined';
 var setImmediate = isWindow && window.setImmediate ? window.setImmediate : (function() {
-    if (typeof process === 'object' && typeof process.nextTick === 'function') {
-        return process.nextTick;
-    } else {
-        return function(fn) {
-            setTimeout(fn, 0);
-        };
-    }
+  if (typeof process === 'object' && typeof process.nextTick === 'function') {
+    return process.nextTick;
+  } else {
+    return function(fn) {
+      setTimeout(fn, 0);
+    };
+  }
 })();
 
 // Loaders storage
 var loaders = {};
 
 function makeLoadPromise(item, increment) {
-    var promise = new Promise(function(resolve, reject) {
-        if (item instanceof Promise) {
-            reject('Item ' + item + ' should not be a promise');
-        } else if (typeof item === 'function') {
-            // If item is a function pass the resolve & reject
-            setImmediate(function() {
-                item.call(this, resolve, reject);
-            });
-        } else {
-            // Prepare variables
-            var regexExt = /(?:\.([^.]+))?$/,
-                ext = regexExt.exec(item)[1],
-                loader = loaders[ext] || (function() {
-                    throw 'No loader for file ' + ext;
-                })();
-            if (typeof loader === 'function') {
-                setImmediate(function() {
-                    loader.call(this, resolve, reject, item);
-                });
-            } else {
-                // Prepare variable for element creation
-                var element = document.createElement(loader.tag || _loaderDefaults.tag),
-                    parent = loader.parent || _loaderDefaults.parent,
-                    attr = loader.attr || _loaderDefaults.attr;
-                // Handle events loaded or error
-                element.onload = function() {
-                    resolve(item);
-                };
-                element.onerror = function() {
-                    reject('Error while loading ' + item);
-                };
-                // Export element for manipulation before attaching to parent
-                loader.config(element);
-                // Inject into document to kick off loading
-                element[attr] = item;
-                document[parent].appendChild(element);
-            }
-        }
-    });
-    if (increment) promise.then(increment);
-    return promise;
+  var promise = new Promise(function(resolve, reject) {
+    if (item instanceof Promise) {
+      reject('Item ' + item + ' should not be a promise');
+    } else if (typeof item === 'function') {
+      // If item is a function pass the resolve & reject
+      setImmediate(function() {
+        item.call(this, resolve, reject);
+      });
+    } else if (item === null) {
+      // Auto resolve if passed a null item
+      resolve();
+    } else {
+      // Prepare variables
+      var regexExt = /(?:\.([^.]+))?$/,
+        ext = regexExt.exec(item)[1],
+        loader = loaders[ext] || (function() {
+          throw 'No loader for file ' + ext;
+        })();
+      if (typeof loader === 'function') {
+        setImmediate(function() {
+          loader.call(this, resolve, reject, item);
+        });
+      } else {
+        // Prepare variable for element creation
+        var element = document.createElement(loader.tag || _loaderDefaults.tag),
+          parent = loader.parent || _loaderDefaults.parent,
+          attr = loader.attr || _loaderDefaults.attr;
+        // Handle events loaded or error
+        element.onload = function() {
+          resolve(item);
+        };
+        element.onerror = function() {
+          reject('Error while loading ' + item);
+        };
+        // Export element for manipulation before attaching to parent
+        loader.config(element);
+        // Inject into document to kick off loading
+        element[attr] = item;
+        document[parent].appendChild(element);
+      }
+    }
+  });
+  if (increment) promise.then(increment);
+  return promise;
 }
 
 
 function makeLoadAsyncPromise(items, increment) {
-    if (typeof items === 'string' || items instanceof String) {
-        // String, converting to array of promises by calling makeLoadPromise function
-        items = [makeLoadPromise(items, increment)];
-    } else if (typeof items === 'function') {
-        // Function, converting to array promises
-        items = [makeLoadPromise(items, increment)];
-    } else {
-        // A valid array of strings or functions, converting to array of promises
-        for (var i = 0; i < items.length; i++) {
-            items[i] = makeLoadPromise(items[i], increment);
-        }
+  if (typeof items === 'string' || items instanceof String) {
+    // String, converting to array of promises by calling makeLoadPromise function
+    items = [makeLoadPromise(items, increment)];
+  } else if (typeof items === 'function') {
+    // Function, converting to array promises
+    items = [makeLoadPromise(items, increment)];
+  } else {
+    // A valid array of strings or functions, converting to array of promises
+    for (var i = 0; i < items.length; i++) {
+      items[i] = makeLoadPromise(items[i], increment);
     }
-    // At this point we should have array of promises
-    return Promise.all(items);
+  }
+  // At this point we should have array of promises
+  return Promise.all(items);
 }
 
 function getPercent(current, total) {
-    return Math.round((current / total) * 100);
+  return Math.round((current / total) * 100);
 }
 
 
 function load(resources, callback, progress) {
-    if (!resources.length > 0) return;
+  if (!resources.length > 0) return;
 
-    function increment(result) {
-        if (progress && !error) progress(getPercent(counter++, total));
-        return result;
+  function increment(result) {
+    if (progress && !error) progress(getPercent(counter++, total));
+    return result;
+  }
+  var error = false,
+    counter = 0,
+    total = resources[0] instanceof Array ? resources[0].length : 1,
+    result = callback ? [] : undefined,
+    prom = makeLoadAsyncPromise(resources[0], increment);
+  resources.forEach(function(item) {
+    if (item !== resources[0]) {
+      total += item instanceof Array ? item.length : 1;
+      prom = prom.then(function(data) {
+        if (callback) result.push(data);
+        return makeLoadAsyncPromise(item, increment);
+      });
     }
-    var error = false,
-        counter = 0,
-        total = resources[0] instanceof Array ? resources[0].length : 1,
-        result = callback ? [] : undefined,
-        prom = makeLoadAsyncPromise(resources[0], increment);
-    resources.forEach(function(item) {
-        if (item !== resources[0]) {
-            total += item instanceof Array ? item.length : 1;
-            prom = prom.then(function(data) {
-                if (callback) result.push(data);
-                return makeLoadAsyncPromise(item, increment);
-            });
-        }
-    });
-    prom.then(function(data) {
-        if (callback) {
-            result.push(data);
-            callback(false, result);
-        }
-    }).catch(function(err) {
-        error = true;
-        if (callback) callback(err);
-    });
-    increment();
-    return prom;
+  });
+  prom.then(function(data) {
+    if (callback) {
+      result.push(data);
+      callback(false, result);
+    }
+  }).catch(function(err) {
+    error = true;
+    if (callback) callback(err);
+  });
+  increment();
+  return prom;
 }
 
 function addLoader(loader) {
-    var i, exts = loader.ext.split(',');
-    for (i = exts.length - 1; i > -1; i--) {
-        loaders[exts[i]] = 'custom' in loader ? loader.custom : loader;
-    }
+  var i, exts = loader.ext.split(',');
+  for (i = exts.length - 1; i > -1; i--) {
+    loaders[exts[i]] = 'custom' in loader ? loader.custom : loader;
+  }
 }
 
 //-----------------------------------------------------------
@@ -126,37 +129,37 @@ function addLoader(loader) {
 //-----------------------------------------------------------
 
 var _loaderDefaults = {
-    tag: 'link',
-    parent: 'head',
-    attr: 'href'
+  tag: 'link',
+  parent: 'head',
+  attr: 'href'
 };
 
 // Javascript
 addLoader({
-    ext: 'js',
-    tag: 'script',
-    parent: 'body',
-    attr: 'src',
-    config: function(el) {
-        el.async = false;
-    }
+  ext: 'js',
+  tag: 'script',
+  parent: 'body',
+  attr: 'src',
+  config: function(el) {
+    el.async = false;
+  }
 });
 
 // Stylesheet
 addLoader({
-    ext: 'css',
-    config: function(el) {
-        el.type = 'text/css';
-        el.rel = 'stylesheet';
-    }
+  ext: 'css',
+  config: function(el) {
+    el.type = 'text/css';
+    el.rel = 'stylesheet';
+  }
 });
 
 // Html
 addLoader({
-    ext: 'html',
-    config: function(el) {
-        el.rel = 'import';
-    }
+  ext: 'html',
+  config: function(el) {
+    el.rel = 'import';
+  }
 });
 
 //-----------------------------------------------------------
@@ -170,5 +173,5 @@ exports.loadOne = makeLoadPromise;
 exports.loadMany = makeLoadAsyncPromise;
 // Export for window
 if (typeof window !== 'undefined' && !window.LoaderJS) {
-    window.LoaderJS = exports;
+  window.LoaderJS = exports;
 }
